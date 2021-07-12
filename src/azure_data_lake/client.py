808a132bb6fc905922ea00b826f425ff7c048921
Adam Bako <bakoad@gmail.com>
2021-07-12 15:43:46 +0200

    intial implementation

---
+++ src/azure_data_lake/client.py
@@ -0,0 +1,26 @@
+from azure.storage.filedatalake import DataLakeServiceClient
+
+
+class AzureDataLakeClient():
+    def __init__(self, account_name, account_key, container_name):
+        service_client = self.initialize_storage_account(account_name, account_key)
+        self.file_system_client = service_client.get_file_system_client(file_system=container_name)
+
+    @staticmethod
+    def initialize_storage_account(storage_account_name, storage_account_key):
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+        return service_client
+
+    def list_directory_contents(self, directory=""):
+        paths = self.file_system_client.get_paths(path=directory)
+        return paths
+
+    def download_file(self, directory, file_name, result_file):
+        directory_client = self.file_system_client.get_directory_client(directory)
+        out_file = open(result_file.full_path, 'wb')
+        file_client = directory_client.get_file_client(file_name)
+        download = file_client.download_file()
+        downloaded_bytes = download.readall()
+        out_file.write(downloaded_bytes)
+        out_file.close()
--- src/component.py
+++ src/component.py
@@ -4,19 +4,14 @@ Template Component main class.
 '''
 
 import logging
-import sys
-
-import logging_gelf.handlers  # noqa
-import requests
-from azure.core.exceptions import ResourceNotFoundError, HttpResponseError, ClientAuthenticationError
-# from azure.storage.blob import BlockBlobService, PublicAccess  # noqa
-from azure.identity import AuthorizationCodeCredential  # noqa
-from azure.storage.blob._shared.authentication import AzureSigningError
-from azure.storage.filedatalake import DataLakeServiceClient
-from kbc.env_handler import KBCEnvHandler
-from kbc.result import KBCTableDef  # noqa
-from kbc.result import ResultWriter  # noqa
-from msal import token_cache  # noqa
+import ntpath
+import dateparser
+from fnmatch2 import fnmatch2
+from datetime import datetime
+from azure.core.exceptions import ServiceRequestError
+from keboola.component.base import ComponentBase, UserException
+from azure_data_lake.client import AzureDataLakeClient
+from azure.storage.filedatalake._generated.models._models_py3 import StorageErrorException
 
 # configuration variables
 KEY_ACCOUNT_NAME = 'account_name'
@@ -24,20 +19,11 @@ KEY_ACCOUNT_KEY = '#account_key'
 KEY_CONTAINER_NAME = 'container_name'
 KEY_NEW_FILE_ONLY = 'new_files_only'
 KEY_FILE = 'file'
-KEY_DEBUG = 'debug'
-KEY_BLOB_DOMAIN = 'blob_domain'
+KEY_FILE_NAME = 'file_name'
+KEY_ADD_TIMESTAMP = "add_timestamp"
 
-MANDATORY_PARS = [
-    KEY_ACCOUNT_NAME,
-    KEY_ACCOUNT_KEY,
-    KEY_CONTAINER_NAME,
-    KEY_FILE
-]
-# list of mandatory parameters => if some is missing,
-# component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = []
+REQUIRED_PARAMETERS = [KEY_ACCOUNT_NAME, KEY_ACCOUNT_KEY, KEY_CONTAINER_NAME, KEY_FILE]
 REQUIRED_IMAGE_PARS = []
-
 
 
 class Component(ComponentBase):
@@ -56,111 +42,92 @@ class Component(ComponentBase):
                          required_image_parameters=REQUIRED_IMAGE_PARS)
 
     def run(self):
-        '''
-        Main execution code
-        '''
+        params = self.configuration.parameters  # noqa
 
-        params = self.cfg_params  # noqa
-        self.validate_config_params(params)
-
-        # Get proper list of tables
-        # now = int(datetime.datetime.utcnow().timestamp())
         account_name = params.get(KEY_ACCOUNT_NAME)
         account_key = params.get(KEY_ACCOUNT_KEY)  # noqa
         container_name = params.get(KEY_CONTAINER_NAME)
         file = params.get(KEY_FILE)
-
-        # Validating user input file configuration
-        if not file:
-            logging.error('File configuration is missing.')
-            sys.exit(1)
+        file_pattern = file.get(KEY_FILE_NAME)
+        add_timestamp = file.get(KEY_ADD_TIMESTAMP, False)
 
         # New file parameter if file configuration exists
         new_file_only = file.get(KEY_NEW_FILE_ONLY)
         logging.info(f'New File Only: {new_file_only}')
 
         # Get state file
+        last_run_timestamp = 0
         state = self.get_state_file()  # noqa
         if state and new_file_only:
-            last_run_timestamp = state.get('lastRunTimestamp')
+            last_run_timestamp = state.get('lastRunTimestamp', 0)
             logging.info(f'Extracting from: {last_run_timestamp}')
 
-        # else:
-        #     last_run_timestamp = None
-        #     state = {}
+        # Initializing file Container client
+        azure_client = AzureDataLakeClient(account_name, account_key, container_name)
 
-        # Initializing Blob Container client
-        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
-            "https", account_name), credential=account_key)
-        file_system_client = service_client.get_file_system_client(file_system=container_name)
-
-        # Validating Credentials
-        # Exceptions Handling
+        file_list = azure_client.list_directory_contents("")
         try:
-            paths = file_system_client.get_paths(max_results=10)
-            logging.info(list(paths))
-        except ClientAuthenticationError:
-            # Credentials and Account Name validation
-            logging.error(
-                'The specified credentials [Account Name] & [Account Key] are invalid.')
-            sys.exit(1)
+            qualified_file_list = self.qualify_files(file_pattern, file_list, last_run_timestamp)
+        except StorageErrorException as e:
+            if e.error.code == "AuthenticationFailed":
+                raise UserException("Invalid client key, please recheck configuration") from e
+            if e.error.code == "FilesystemNotFound":
+                raise UserException("Invalid container name, please recheck configuration") from e
+        except ServiceRequestError as e:
+            raise UserException("Failed to connect, please check your Account Name in the configuration") from e
 
-        except ResourceNotFoundError as e:
-            # Container validation
-            logging.exception(e)
-            sys.exit(1)
-        except HttpResponseError as e:
-            logging.exception(e)
-            sys.exit(1)
+        for file in qualified_file_list:
+            file_name = ntpath.basename(file["name"])
+            out_file_name = file["name"].replace("/", "_")
+            if add_timestamp:
+                out_file_name = "_".join([str(file["last_modified"]), out_file_name])
+            file_directory = ntpath.dirname(file["name"])
+            out_file = self.create_out_file_definition(name=out_file_name)
+            azure_client.download_file(file_directory, file_name, out_file)
 
-        except Exception as e:
-            # If there are any other errors, reach out support
-            logging.exception(e)
-            logging.error('Please validate your [Account Name].')
-            sys.exit(1)
+        state = {'lastRunTimestamp': int(datetime.utcnow().timestamp())}
+        self.write_state_file(state)
+
+    def qualify_files(self, file_pattern, file_list, last_run_timestamp):
+        qualified_files = []
+        qualified_files_short = []
+
+        for file in file_list:
+
+            file_name = file.name
+            file_last_modified = dateparser.parse(file.last_modified)
+            file_last_modified = int(datetime.timestamp(file_last_modified))
+
+            match_bool = fnmatch2(file_name, file_pattern)
+            logging.debug(
+                f'Matched: {match_bool} | {file_pattern} <> {file_name}')
+
+            if match_bool is True and ".csv" in file_name:
+                file_obj = {}
+                if last_run_timestamp:
+                    if file_last_modified >= int(last_run_timestamp):
+                        logging.debug('New File: True')
+                        file_obj['name'] = file_name
+                        file_obj['last_modified'] = file_last_modified
+                        qualified_files.append(file_obj)
+                        qualified_files_short.append(file_name)
+
+                    else:
+                        logging.debug('New File: False')
+
+                else:
+                    # qualified_files += [file_name]
+                    file_obj['name'] = file_name
+                    file_obj['last_modified'] = file_last_modified
+                    qualified_files.append(file_obj)
+                    qualified_files_short.append(file_name)
+
+        logging.info(f'Number of qualified file files: {len(qualified_files)}')
+        logging.debug(f'Qualified Files: {qualified_files_short}')
+
+        return qualified_files
 
 
-    def validate_config_params(self, params):
-        '''
-        Validating if input configuration contain everything needed
-        '''
-
-        # Credentials Conditions
-        # Validate if config is blank
-        if params == {}:
-            logging.error(
-                'Configurations are missing. Please configure your component.')
-            sys.exit(1)
-
-        # Validate if the configuration is empty
-        empty_config = {
-            'account_name': '',
-            '#account_key': '',
-            'container_name': ''
-        }
-        if params == empty_config:
-            logging.error(
-                'Configurations are missing. Please configure your component.')
-            sys.exit(1)
-
-        # Validating config parameters
-        if params[KEY_ACCOUNT_NAME] == '' or params[KEY_ACCOUNT_KEY] == '':
-            logging.error(
-                "Credientials missing: Account Name, Access Key...")
-            sys.exit(1)
-        if params[KEY_CONTAINER_NAME] == '':
-            logging.error(
-                "Blob Container name is missing, check your configuration.")
-            sys.exit(1)
-
-
-
-
-
-
-"""
-        Main entrypoint
-"""
 if __name__ == "__main__":
     try:
         comp = Component()
